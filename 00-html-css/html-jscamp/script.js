3b8f991e182e59d06048af41376811e43984365f
DanielBarri <danie.barri@gmail.com>
2025-10-08 20:14:49 -0600

    Avance dia 5

---
+++ 00-html-css/html-jscamp/script.js
@@ -0,0 +1,11 @@
+const boton = document.querySelectorAll(".apply-button");
+
+/*
+botones.forEach((boton) => {
+    botones.addEventListener("click", function () {
+        boton.textContent = "Aplicado!";
+        boton.classList.add("is-applied");
+        boton.diabled = true;
+    });
+});
+*/
